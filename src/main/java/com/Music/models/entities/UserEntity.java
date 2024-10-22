ffb29f8c23449d321891851ce7a37bc127afd0f2
Duj0 <162356980+Duj0@users.noreply.github.com>
2024-10-22 15:05:59 +0300

    Add files via upload

---
+++ src/main/java/com/Music/models/entities/UserEntity.java
@@ -0,0 +1,63 @@
+package com.Music.models.entities;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class UserEntity extends BaseEntity {
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<UserRoleEntity> roles = new ArrayList<>();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserEntity setUsername(String name) {
+        this.username = name;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserEntity setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public UserEntity setFullName(String fullname) {
+        this.fullName = fullname;
+        return this;
+    }
+
+    public List<UserRoleEntity> getRoles() {
+        return roles;
+    }
+
+    public UserEntity setRoles(List<UserRoleEntity> roles) {
+        this.roles = roles;
+        return this;
+    }
+
+    public UserEntity addRole(UserRoleEntity roleEntity) {
+        this.roles.add(roleEntity);
+        return this;
+    }
+}
