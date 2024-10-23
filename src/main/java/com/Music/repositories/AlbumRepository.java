a72a9a411a82082d99442b60ce5e13fc1c087223
Duj0 <162356980+Duj0@users.noreply.github.com>
2024-10-23 21:42:09 +0300

    Create AlbumRepository

---
+++ src/main/java/com/Music/repositories/AlbumRepository.java
@@ -0,0 +1,7 @@
+package com.Music.repositories;
+
+import com.Music.models.entities.AlbumEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//public record AlbumRepository implements JpaRepository<AlbumEntity, Long> {
+//}
