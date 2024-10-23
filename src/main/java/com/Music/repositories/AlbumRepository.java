206bbff4dd2683a24220d281d4f41c466a1d9eac
Denis <125385668+denjiyy@users.noreply.github.com>
2024-10-23 21:40:35 +0300

    Delete src/main/java/com/Music/repositories/AlbumRepository.java

--- src/main/java/com/Music/repositories/AlbumRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Music.repositories;
-
-import com.Music.models.entities.AlbumEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//public record AlbumRepository implements JpaRepository<AlbumEntity, Long> {
-//}
